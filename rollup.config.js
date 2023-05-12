509585fa70dbcdd13608e5824ccbeee39a2ae5a8
John Hooks <bitmachina@outlook.com>
2023-05-12 15:45:19 -0700

    fix: add back CommonJS output (#11)
    * fix: add back cjs-output
    
    * Update package.json
    
    Co-authored-by: Andrew Duthie <andrew@andrewduthie.com>
    
    * fix: remove replacement of process.env in cjs output
    
    * chore: apply @aduth's suggestion
    
    ---------
    
    Co-authored-by: Andrew Duthie <andrew@andrewduthie.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,34 @@
 import replace from '@rollup/plugin-replace';
 
-export default {
-	input: 'index.js',
-	output: {
-		format: 'es',
-		file: 'dist/index.js',
+export default [
+	{
+		input: 'index.js',
+		output: {
+			format: 'es',
+			file: 'dist/index.js',
+		},
+		plugins: [
+			replace({
+				preventAssignment: true,
+				values: {
+					'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+				},
+			}),
+		],
 	},
-	plugins: [
-		replace({
-			preventAssignment: true,
-			values: {
-				'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-			},
-		}),
-	],
-};
+	{
+		input: 'index.js',
+		output: {
+			format: 'cjs',
+			file: 'dist/index.cjs',
+		},
+		plugins: [
+			replace({
+				preventAssignment: true,
+				values: {
+					'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+				},
+			}),
+		],
+	},
+];
